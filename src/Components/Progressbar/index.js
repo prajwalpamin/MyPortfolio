7607b960823fb8e4432e57e1e6bc5dc9138c3af9
Prajwal <prajwalpamin77@gmail.com>
2022-02-07 08:04:21 +0530

    animation

--- src/Components/Progressbar/index.js
+++ src/Components/Progressbar/index.js
@@ -10,15 +10,7 @@ const Progressbar = (props) => {
      </div>
 
    </div>
-    // <div className="performance-bar">
-    //   <div className="card">
-    //     <h2>{props.name}</h2>
-    //     {/* <i class="fab fa-html5"></i> */}
-    //     <div className="bar">
-    //       <div className="inner-bar" style={{width:`${props.width}`}}></div>
-    //     </div>
-    //   </div>
-    // </div>
+   
   );
 };
 export default Progressbar;
